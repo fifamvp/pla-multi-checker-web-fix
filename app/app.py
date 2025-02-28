3e6f06778643d1dd003f807f3f0b707106cf6446
fifamvp <fifa.m.v.p@outlook.com>
2025-03-01 00:27:48 +0800

    Let config control the APP_MODE

--- app/app.py
+++ app/app.py
@@ -1,22 +1,23 @@
 from enum import Enum
+import json, os
 
 class AppMode(Enum):
     WEB = "WEB"
     LOCAL = "LOCAL"
     DEV = "DEV"
 
+config = json.load(open(os.path.dirname(__file__) + "/../config.json"))
+
+if 'APP_MODE' not in config:
+    print("Warning: No APP_MODE in config.json")
+
 _root = {
-    AppMode.WEB: '/home/pla-multi-checker-web-fix/',
+    AppMode.WEB: config['WEB_PATH'] + 'pla-multi-checker-web-fix/',
     AppMode.LOCAL: './pla-multi-checker-web-fix/',
     AppMode.DEV: './pla-multi-checker-web-fix/'
 }
 
-_resources = {
-    AppMode.WEB: '/home/pla-multi-checker-web/static/',
-    AppMode.LOCAL: './pla-multi-checker-web-fix/static/',
-    AppMode.DEV: './static/'
-}
 
-APP_MODE = AppMode.LOCAL
-RESOURCE_PATH = _resources[APP_MODE]
+APP_MODE = AppMode(config['APP_MODE'])
 ROOT_PATH = _root[APP_MODE]
+RESOURCE_PATH = ROOT_PATH + 'static/'
